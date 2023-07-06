b979ed806bd9215e3c98f9d37abbdfa63fd0e768
Bernard Czenkusz <bernie@skipole.co.uk>
2023-07-06 13:59:23 +0100

    Working on ipiserver

--- indipydriver/ipyserver.py
+++ indipydriver/ipyserver.py
@@ -36,8 +36,13 @@ class IPyServer:
         self.serverreaderque = asyncio.Queue(6)
 
         if maxconnections<1 or maxconnections>10:
-            raise ValueError("maxconnections should be a number between 1 and 10")
+            raise ValueError("maxconnections should be a number between 1 and 10 inclusive.")
         self.maxconnections = maxconnections
+
+        self.connectionpool = []
+        for clientconnection in range(0, maxconnections):
+            self.connectionpool.append(ClientConnection(self.serverreaderque, self.serverwriterque))
+
 
         for driver in drivers:
             if not isinstance(driver, IPyDriver):
@@ -46,7 +51,7 @@ class IPyServer:
             if not driver.comms is None:
                  raise RuntimeError("A driver communications method has already been set, there can only be one")
             # an instance of _DriverComms is created for each driver
-            driver.comms = _DriverComms(self.serverwriterque)
+            driver.comms = _DriverComms(self.serverwriterque, self.connectionpool)
         self.drivers = drivers
         self.host = host
         self.port = port
@@ -62,17 +67,16 @@ class IPyServer:
 
     async def handle_data(self, reader, writer):
         "Used by asyncio.start_server, called to handle a client connection"
-        rx = Port_RX(reader)
-        tx = Port_TX(writer)
-        try:
-            txtask = asyncio.create_task(tx.run_tx(self.serverwriterque))
-            rxtask = asyncio.create_task(rx.run_rx(self.serverreaderque))
-            await txtask
-            await rxtask
-        except ConnectionResetError:
-            txtask.cancel()
-            rxtask.cancel()
 
+        for clientconnection in self.connectionpool:
+            if not clientconnection.connected:
+                # this clientconnection is available
+                await clientconnection.handle_data(reader, writer)
+                break
+        else:
+            # no clientconnection is available
+            writer.close()
+            await writer.wait_closed()
 
     async def asyncrun(self):
         """Gathers tasks to be run simultaneously"""
@@ -104,12 +108,15 @@ class _DriverComms:
        be taken from the writerque and transmitted to the client by placing it
        into the serverwriterque"""
 
-    def __init__(self, serverwriterque):
+    def __init__(self, serverwriterque, connectionpool):
         # self.rxque will have data received from the network
         # inserted into it from the IPyServer.copyreceivedtodriversrxque()
         # method
         self.rxque = asyncio.Queue(6)
         self.serverwriterque = serverwriterque
+        # connectionpool is a list of ClientConnection objects, which is used
+        # to test if a client is connected
+        self.connectionpool = connectionpool
         # self.connected is read by the driver, and in this case is always True
         # as the driver is connected to IPyServer, which handles snooping traffic,
         # even if no client is connected
@@ -137,9 +144,52 @@ class _DriverComms:
             await asyncio.sleep(0)
             xmldata = await writerque.get()
             # should check if this driver wants to snoop
-            await self.serverwriterque.put(xmldata)
-            # task completed
-            self.writerque.task_done()
+
+            # If no clients are connected, do not put this data into
+            # the serverwriterque
+            for clientconnection in self.connectionpool:
+                if clientconnection.connected:
+                    await self.serverwriterque.put(xmldata)
+                    # task completed
+                    writerque.task_done()
+                    break
+            else:
+                # no client connected, discard this data
+                # if xmldata is a file pointer, close it
+                if (xmldata.tag == "setBLOBVector") and len(xmldata):
+                    # xmldata is a setBLOBVector containing blobs
+                    for oneblob in xmldata.iter('oneBLOB'):
+                        # get the filepointer
+                        fp = oneblob.text
+                        if hasattr(fp 'close'):
+                            fp.close()
+
+
+
+class ClientConnection:
+
+    "Handles a client connection"
+
+    def __init__(self, serverreaderque, serverwriterque):
+        self.serverreaderque = serverreaderque
+        self.serverwriterque = serverwriterque
+        # self.connected is True if this pool object is running a connection
+        self.connected = False
+
+    async def handle_data(self, reader, writer):
+        "Used by asyncio.start_server, called to handle a client connection"
+        self.connected = True
+        rx = Port_RX(reader)
+        tx = Port_TX(writer)
+        try:
+            txtask = asyncio.create_task(tx.run_tx(self.serverwriterque))
+            rxtask = asyncio.create_task(rx.run_rx(self.serverreaderque))
+            await txtask
+            await rxtask
+        except ConnectionResetError:
+            self.connected = False
+            txtask.cancel()
+            rxtask.cancel()
 
 
 
