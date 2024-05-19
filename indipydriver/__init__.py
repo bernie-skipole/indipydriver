db38b309a8ebac19bff43a79e2d73020808d919d
Bernard Czenkusz <bernie@skipole.co.uk>
2024-05-19 10:11:31 +0100

    Added logging nullhandler

--- indipydriver/__init__.py
+++ indipydriver/__init__.py
@@ -1,5 +1,9 @@
 
 
+
+import logging
+logging.getLogger('indipydriver').addHandler(logging.NullHandler())
+
 from .ipydriver import IPyDriver, Device
 from .propertyvectors import SwitchVector, LightVector, TextVector, BLOBVector, NumberVector
 from .propertymembers import SwitchMember, LightMember, TextMember, BLOBMember, NumberMember
