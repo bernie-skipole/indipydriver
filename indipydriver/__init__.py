036277acdd95d98030edc8af663e17c1033e48a4
Bernard Czenkusz <bernie@skipole.co.uk>
2025-02-09 22:00:11 +0000

    Added python version check

--- indipydriver/__init__.py
+++ indipydriver/__init__.py
@@ -1,9 +1,13 @@
 
+import sys
 
 from .ipydriver import IPyDriver, Device, getfloat
 from .propertyvectors import SwitchVector, LightVector, TextVector, BLOBVector, NumberVector
 from .propertymembers import SwitchMember, LightMember, TextMember, BLOBMember, NumberMember
 from .events import getProperties, enableBLOB, newSwitchVector, newTextVector, newNumberVector, newBLOBVector, Message, delProperty, defSwitchVector, defTextVector, defNumberVector, defLightVector, defBLOBVector, setSwitchVector, setTextVector, setNumberVector, setLightVector, setBLOBVector
 from .ipyserver import IPyServer
+
+if sys.version_info < (3, 10):
+    raise ImportError('indipyterm requires Python >= 3.10')
 
 version = "2.1.0"
