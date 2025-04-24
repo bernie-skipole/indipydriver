24c2a91ebb09c1237f79bb362c8398f53903cb7c
Bernard Czenkusz <bernie@skipole.co.uk>
2025-04-24 12:16:26 +0100

    Now version 2.3.4, added __all__ and some refactoring

--- indipydriver/__init__.py
+++ indipydriver/__init__.py
@@ -10,4 +10,12 @@ from .ipyserver import IPyServer
 if sys.version_info < (3, 10):
     raise ImportError('indipyterm requires Python >= 3.10')
 
-version = "2.3.3"
+version = "2.3.4"
+
+
+__all__ = ["version", "IPyDriver", "Device", "getfloat", "IPyServer",
+           "SwitchVector", "LightVector", "TextVector", "BLOBVector", "NumberVector",
+           "SwitchMember", "LightMember", "TextMember", "BLOBMember", "NumberMember",
+           "getProperties", "enableBLOB", "newSwitchVector", "newTextVector", "newNumberVector", "newBLOBVector",
+           "Message", "delProperty", "defSwitchVector", "defTextVector", "defNumberVector", "defLightVector", "defBLOBVector",
+           "setSwitchVector", "setTextVector", "setNumberVector", "setLightVector", "setBLOBVector"]
