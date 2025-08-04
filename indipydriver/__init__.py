a71be0b35582400cbee58c666750f128c3a30bb4
Bernard Czenkusz <bernie@skipole.co.uk>
2025-08-04 10:25:40 +0100

    Fixed another bug

--- indipydriver/__init__.py
+++ indipydriver/__init__.py
@@ -4,18 +4,18 @@ import sys
 from .ipydriver import IPyDriver, Device
 from .propertyvectors import SwitchVector, LightVector, TextVector, BLOBVector, NumberVector
 from .propertymembers import SwitchMember, LightMember, TextMember, BLOBMember, NumberMember, getfloat
-from .events import getProperties, enableBLOB, newSwitchVector, newTextVector, newNumberVector, newBLOBVector, Message, delProperty, defSwitchVector, defTextVector, defNumberVector, defLightVector, defBLOBVector, setSwitchVector, setTextVector, setNumberVector, setLightVector, setBLOBVector
+from .events import getProperties, newSwitchVector, newTextVector, newNumberVector, newBLOBVector, Message, delProperty, defSwitchVector, defTextVector, defNumberVector, defLightVector, defBLOBVector, setSwitchVector, setTextVector, setNumberVector, setLightVector, setBLOBVector
 from .ipyserver import IPyServer
 
 if sys.version_info < (3, 10):
     raise ImportError('indipyterm requires Python >= 3.10')
 
-version = "2.4.1"
+version = "2.4.2"
 
 
 __all__ = ["version", "IPyDriver", "Device", "getfloat", "IPyServer",
            "SwitchVector", "LightVector", "TextVector", "BLOBVector", "NumberVector",
            "SwitchMember", "LightMember", "TextMember", "BLOBMember", "NumberMember",
-           "getProperties", "enableBLOB", "newSwitchVector", "newTextVector", "newNumberVector", "newBLOBVector",
+           "getProperties", "newSwitchVector", "newTextVector", "newNumberVector", "newBLOBVector",
            "Message", "delProperty", "defSwitchVector", "defTextVector", "defNumberVector", "defLightVector", "defBLOBVector",
            "setSwitchVector", "setTextVector", "setNumberVector", "setLightVector", "setBLOBVector"]
